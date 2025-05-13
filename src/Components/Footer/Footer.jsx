cf29183e64e189d459820964aa081bb4ea6318cb
seblewongel-kassa <titigswk@gmail.com>
2025-05-13 09:56:43 -1000

    Header & Footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import "./Footer.css"
+import FacebookIcon from "@mui/icons-material/Facebook"; 
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+const Footer = () => {
+  return (
+    <div>
+      <div className="Footer_outer_container">
+        <div className="Footer_inner_container">
+          <div className="Footer_link">
+            <FacebookIcon/>
+            <InstagramIcon/>
+            <TwitterIcon/>
+            <YouTubeIcon/>
+          </div>
+
+          <div className="Footer_data">
+            <div>
+              <ul>
+                <li>Audio Description</li>
+                <li>Investor Relation</li>
+                <li>Privacy</li>
+                <li>Contact Us</li>
+              </ul>
+            </div>
+            <div>
+              <ul>
+                <li>Help Center</li>
+                <li>Legal Notices</li>
+                <li>Do not sell or share My Personal Information</li>
+                <li></li>
+              </ul>
+            </div>
+            <div>
+              <ul>
+                <li>Gift Cards</li>
+                <li>Nateflix Shope</li>
+                <li>Cookie Preferances</li>
+                <li>Ad Coices</li>
+              </ul>
+            </div>
+            <div>
+              <ul>
+                <li>Media Center</li>
+                <li>Terms Of Use</li>
+                <li>Corporate Information</li>
+                
+              </ul>
+            </div>
+          </div>
+
+          <div className="service_para">
+            <p>Service Code</p>
+          </div>
+
+          <div className="copy_right">
+            &copy;1997_2025 Netflix,inc.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./Header.css";
+import netflix from "../../assets/Netflix.jpeg";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+
+const Header = () => {
+  return (
+    <div className="header-outer-container">
+      <div className="header-container">
+        <div className="header-left">
+          <ul>
+            <li>
+              <img src={netflix} alt="Netflix" />
+            </li>
+            <li>Home</li>
+            <li>TVshows</li>
+            <li>Movies</li>
+            <li>Latest</li>
+            <li>MyList</li>
+            <li>Browse by Languages</li>
+          </ul>
+        </div>
+        <div className="header-right">
+          <ul>
+            <li>
+              <SearchIcon />
+            </li>
+            <li>
+              <NotificationsNoneIcon />
+            </li>
+            <li>
+              <AccountBoxIcon />
+            </li>
+            <li>
+              <ArrowDropDownIcon />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
