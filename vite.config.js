87d444fa234400ab64b6af9f9f16dc43e0c307d2
seblewongel-kassa <titigswk@gmail.com>
2025-05-19 08:36:10 -1000

    last commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Netflix-by-Seble",
+  base: "/Netflix-by-Seble/",
 });
